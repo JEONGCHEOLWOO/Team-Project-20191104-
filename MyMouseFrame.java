afbbea9783f2383361f4a3df55b249412c762c77
SEONMIN99 <seonmin99@naver.com>
2019-11-10 23:17:55 +0900

    코드 분석 & 주석문 기입

--- MyMouseFrame.java
+++ MyMouseFrame.java
@@ -7,26 +7,27 @@ import javax.swing.*;
  * @author (2018315031 박성현, 2018315040 김선민, 2018315034 정철우)
  * @version (2019/11/04)
  */
-public class MyMouseFrame extends JFrame
+public class MyMouseFrame extends JFrame // JFrame 클래스 상속받기
 {
     public JPanel jp;
-    private JLabel jl = new JLabel("No Mouse Event");
+    private JLabel jl = new JLabel("No Mouse Event"); //레이블 생성
 
     public MyMouseFrame(){
-        this.setTitle("MouseListener & MouseMotionListener");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(300, 400);
-        
-        JPanel jp = new JPanel();
-        
-        this.add(jp);
+        this.setTitle("MouseListener & MouseMotionListener"); //프레임의 title 이름설정
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //종료시 프로세스까지 모두 종료
+        this.setSize(300, 400); // 프레임 사이즈 설정
+
+        JPanel jp = new JPanel(); //패널 생성
+
+        this.add(jp); // 프레임에 패널을 붙임
         MyMouseListener ml = new MyMouseListener(jl,jp);
         jp.addMouseListener(ml);
         jp.addMouseMotionListener(ml);
-        
-        jp.add(jl);
+
+        jp.add(jl); //패널에 레이블 추가
         jl.setSize(300,200);
-        this.setVisible(true);
+        //jl.setPreferredSize(new Dimension(200, 200)); //label 위치 변경
+        this.setVisible(true); //화면에 나타냄
     }
 
 }
--- MyMouseListener.java
+++ MyMouseListener.java
@@ -7,7 +7,8 @@ import java.awt.*;
  * @author (2018315031 박성현, 2018315040 김선민, 2018315034 정철우)
  * @version (2019/11/04)
  */
-public class MyMouseListener extends MouseAdapter implements MouseListener
+public class MyMouseListener extends MouseAdapter implements MouseListener 
+// MouseListener인 인터페이스 상속(inplements), MouseAdapter 클래스 상속(extends)
 {
     JPanel jp;
     JLabel jl;
@@ -15,25 +16,33 @@ public class MyMouseListener extends MouseAdapter implements MouseListener
         this.jp = jp;
         this.jl = jl;
     }
-    public void mouseClicked(MouseEvent e){}
-    public void mouseEntered(MouseEvent e){
-        JPanel jp = (JPanel)e.getSource();
-        jp.setBackground(Color.CYAN);
+
+    // 인터페이스 MouseListener 내의 메소드
+    public void mouseClicked(MouseEvent e){} // 패널 안에서 마우스 버튼를 클릭할 때
+
+    public void mouseEntered(MouseEvent e){ // 마우스가 패널 안에 들어갈 때 
+        JPanel jp = (JPanel)e.getSource(); // 이벤트를 발생시킨 객체의 위치값 가져오기
+        jp.setBackground(Color.CYAN); // 패널의 배경색 설정
     }
-    public void mouseExited(MouseEvent e){
-        JPanel jp = (JPanel)e.getSource();
-        jp.setBackground(Color.YELLOW);
+
+    public void mouseExited(MouseEvent e){ // 마우스가 패널 밖으로 나갈 때
+        JPanel jp = (JPanel)e.getSource(); // 이벤트를 발생시킨 객체의 위치값 가져오기
+        jp.setBackground(Color.YELLOW); // 패널의 배경색 설정
     }
-    public void mousePressed(MouseEvent e){
-        jl.setText("MousePressed (" + e.getX() + "," + e.getY() + ")");
-    }
-    public void mouseReleased(MouseEvent e){
-        jl.setText("MouseReleased (" + e.getX() + "," + e.getY() + ")");
-    }
-    public void mouseDragged(MouseEvent e){
+
+    public void mousePressed(MouseEvent e){ // 패널 안에서 마우스 버튼을 눌렀을 떼
+        jl.setText("MousePressed (" + e.getX() + "," + e.getY() + ")"); 
+    } // label 변경 / X, Y좌표 가져오기
+
+    public void mouseReleased(MouseEvent e){ // 패널 안에서 마우스 버튼을 누르고 놓았을 때
+        jl.setText("MouseReleased (" + e.getX() + "," + e.getY() + ")"); 
+    } // label 변경 / X, Y좌표 가져오기
+
+    public void mouseDragged(MouseEvent e){ // 패널 안에서 드래그할 때(마우스를 누른 채 움직인다) 
         jl.setText("MouseDragged (" + e.getX() + "," + e.getY() + ")");
-    }
-    public void mouseMoved(MouseEvent e){
+    } // label 변경 / X, Y좌표 가져오기
+
+    public void mouseMoved(MouseEvent e){ // 마우스 커서가 패널 안에서 이동할 때
         jl.setText("MouseMoved (" + e.getX() + "," + e.getY() + ")");
-    }
+    } // label 변경 / X, Y좌표 가져오기
 }
